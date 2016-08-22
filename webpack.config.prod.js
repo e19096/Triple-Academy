fc8c1bf9570f7deadf242b4c673c3819b2e54798
Esther Pong <e.pong12@gmail.com>
2016-08-21 22:04:45 -0400

    update package.json

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,8 +6,8 @@ module.exports = {
   context: __dirname,
   entry: "./lib/main.js",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "./lib/bundle.js",
+    path: path.join(__dirname, 'lib'),
+    filename: "bundle.js",
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
