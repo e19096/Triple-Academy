f4e67c40718428fd9088c7f17381914529b9469a
Esther Pong <e.pong12@gmail.com>
2016-08-21 20:49:58 -0400

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,6 @@ module.exports = {
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ]
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,18 +22,6 @@ module.exports = {
      }
    })
  ],
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ]
